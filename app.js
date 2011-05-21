1631183d509c2f5f554b9847f38e507e68f706b9
ilstar <xingqinglzq@gmail.com>
2011-05-21 08:07:34 +0800

    draw the chessboard

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // load plugins
 var express = require('express');
 var app = express.createServer();
+require('ejs');
 
+// settings
+app.set( "view engine", "ejs" );
+app.set('view options', {
+  layout: false,
+});
+
+// controllers and actions
 app.get('/', function(req, res) {
-  res.send("hello world!")
+  res.render('index', {locals: {name: 'haha'}});
+  // res.send("haha");
 });
 
 app.listen(3000);
